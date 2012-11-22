d786f99fe29696e90d2f62637ae66c45705a145b
DarkAngel <darkangel39@abv.bg>
2012-11-22 22:22:14 +0200

    Added spells to vehicles used in Pilgrim's Bounty + close #8.

--- Pilgrim's Bounty/Pilgrim's Bounty Spawn.sql
+++ Pilgrim's Bounty/Pilgrim's Bounty Spawn.sql
@@ -4645,6 +4645,24 @@ UPDATE `creature_proto` SET `auras` = '33209' WHERE `entry`=19178;
 
 UPDATE `creature_names` SET `male_displayid`=29205 WHERE `entry` IN (34812,34822,34823,34819,34824);
 
+SET @PASS_PIE = 66260;
+SET @PASS_TURKEY = 66250;
+SET @PASS_POTATO = 66262;
+SET @PASS_STUFFING = 66259;
+SET @PASS_CRANBERRY = 66261;
+SET @FEAST_PIE = 61787;
+SET @FEAST_TURKEY = 61784;
+SET @FEAST_POTATO = 61786;
+SET @FEAST_STUFFING = 61788;
+SET @FEAST_CRANBERRY = 61785;
+SET @RIDE_VEHICLE = 65403;
+
+UPDATE `creature_proto` SET `spell1`=@PASS_CRANBERRY, `spell2`=@FEAST_TURKEY, `spell3`=@FEAST_STUFFING, `spell4`=@FEAST_POTATO, `spell5`=@FEAST_PIE, `spell8`=@RIDE_VEHICLE WHERE `entry`=34823; -- The Cranberry Chair
+UPDATE `creature_proto` SET `spell1`=@PASS_PIE, `spell2`=@FEAST_TURKEY, `spell3`=@FEAST_CRANBERRY, `spell4`=@FEAST_POTATO, `spell5`=@FEAST_STUFFING, `spell8`=@RIDE_VEHICLE WHERE `entry`=34822; -- The Pie Chair
+UPDATE `creature_proto` SET `spell1`=@PASS_POTATO, `spell2`=@FEAST_TURKEY, `spell3`=@FEAST_CRANBERRY, `spell4`=@FEAST_POTATO, `spell5`=@FEAST_PIE, `spell8`=@RIDE_VEHICLE WHERE `entry`=34824; -- The Sweet Potato Chair
+UPDATE `creature_proto` SET `spell1`=@PASS_STUFFING, `spell2`=@FEAST_TURKEY, `spell3`=@FEAST_CRANBERRY, `spell4`=@FEAST_POTATO, `spell5`=@FEAST_PIE, `spell8`=@RIDE_VEHICLE WHERE `entry`=34819; -- The Stuffing Chair
+UPDATE `creature_proto` SET `spell1`=@PASS_TURKEY, `spell2`=@FEAST_CRANBERRY, `spell3`=@FEAST_STUFFING, `spell4`=@FEAST_POTATO, `spell5`=@FEAST_PIE, `spell8`=@RIDE_VEHICLE  WHERE `entry`=34812; -- The Turkey Chair
+
 DELETE FROM `creature_quest_finisher` WHERE `quest` IN (14022,14023,14024,14028,14030,14033,14035,14036,14037,14040,14041,14043,14044,14047,14048,14051,14053,14054,14055,14058,14059,14060,14061,14062,14064,14065);
 INSERT INTO `creature_quest_finisher` (`id`, `quest`) VALUES
 	(34644,14024),
