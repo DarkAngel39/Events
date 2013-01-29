3a7793920b57708f26b400b9d1e4750a1fdbd5b1
DarkAngel <darkangel39@abv.bg>
2013-01-29 14:42:05 +0200

    Finished updating Love is in the Air event.

--- Love is in the Air/Despawn_Love_is_in_the_Air.sql
+++ Love is in the Air/Despawn_Love_is_in_the_Air.sql
@@ -1,4 +1,2 @@
-DELETE FROM `creature_quest_starter` WHERE `id` IN ('279', '5204', '6740', '6741') AND `quest` IN ('9025', '8980', '9027', '8983');
-DELETE FROM `creature_quest_finisher` WHERE `id` IN ('279', '5204', '6740', '6741') AND `quest` IN ('9025', '8980', '9027', '8983');
 DELETE FROM `creature_spawns` WHERE `id` BETWEEN '400059' AND '400181';
 DELETE FROM `gameobject_spawns` WHERE `id` BETWEEN '210001' AND '211444';
