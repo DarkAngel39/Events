18d745f4ecc0395f3b9953659dad78767b59d372
DarkAngel <darkangel39@abv.bg>
2013-04-10 17:16:15 +0300

    Some more waypoints for Mulgore's Darkmoon Fair.

--- Darkmoon Faire/Mulgore/Despawns_Mulgore.sql
+++ Darkmoon Faire/Mulgore/Despawns_Mulgore.sql
@@ -1,4 +1,4 @@
 DELETE FROM `gameobject_spawns` WHERE `id` BETWEEN '505136' AND '505271';
 DELETE FROM `creature_spawns` WHERE `id` BETWEEN '400132' AND '400161';
 DELETE FROM `creature_formations` WHERE `spawn_id`=400151;
-DELETE FROM `creature_waypoints` WHERE `spawnid`=400157;
+DELETE FROM `creature_waypoints` WHERE `spawnid` IN (400157,400139,400142,400161);
