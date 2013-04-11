5efda9e453dcb635da773cb22c5649f74e34b69e
DarkAngel <darkangel39@abv.bg>
2013-04-11 11:20:47 +0300

    Darkmoon Fare: Elwynn Forest. Add some pathing and other corrections.

--- Darkmoon Faire/Elwynn Forest/Depawn_Elwynn_Forest.sql
+++ Darkmoon Faire/Elwynn Forest/Depawn_Elwynn_Forest.sql
@@ -1,2 +1,3 @@
 DELETE FROM `creature_spawns` WHERE `id` BETWEEN '400078' AND '400104';
 DELETE FROM `gameobject_spawns` WHERE `id` BETWEEN '505018' AND '505135';
+DELETE FROM `creature_waypoints` WHERE `spawnid` IN (400098,400099);
