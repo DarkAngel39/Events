db43c3e938597c709517ea08eadc50fffd51c9aa
DarkAngel <darkangel39@abv.bg>
2013-07-06 00:18:26 +0300

    Added the event controller part to Elwynn Forest Building and also added the Elemental Invasion event. It may also be incomplete. Did this event actually happen on 3.3.5 or 4.0.1?

--- Darkmoon Faire/Elwynn Forest Building/Elwynn Forest Building Spawn.sql
+++ Darkmoon Faire/Elwynn Forest Building/Elwynn Forest Building Spawn.sql
@@ -1,3 +1,12 @@
+CREATE TABLE IF NOT EXISTS `active_event_id` (
+  `active_event` int(10) unsigned NOT NULL,
+  `name` text COLLATE latin1_bin NOT NULL,
+  PRIMARY KEY (`active_event`)
+);
+
+DELETE FROM `active_event_id` WHERE `active_event`=36;
+INSERT INTO `active_event_id` (`active_event`, `name`) VALUES (36,"Darkmoon Faire: Elwynn Forest Building");
+
 SET @SPID := 400000;
 SET @GOID := 400000;
 
