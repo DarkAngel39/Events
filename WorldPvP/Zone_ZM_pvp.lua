b3c0c2d8935acb5b5ed75b0e1f76830bd7f4ad2b
DarkAngel <darkangel39@abv.bg>
2013-12-12 23:01:49 +0200

    Some more work on Zangarmarsh.

--- WorldPvP/Zone_ZM_pvp.lua
+++ WorldPvP/Zone_ZM_pvp.lua
@@ -11,6 +11,7 @@ local BAR_STATUS_1 =	(100 - BAR_NEUTRAL)/2
 local add_banner = false
 local h_t = 0
 local a_t = 0
+controll_team = 10
 
 local ZM_BUFF =			33779
 local ZM_STANDARD_A =	32430
@@ -20,6 +21,10 @@ local ZM_HK =			32158
 
 local HORDE_SCOUT =		18564
 local ALLIANCE_SCOUT =	18581
+
+local A_BANNER = 182527
+local H_BANNER = 182528
+local N_BANNER = 182529
 
 local ZONE_ZM = 3521
 
@@ -33,11 +38,7 @@ local capturepoint_data = {
 
 local buff_zone = {3521, 3607, 3717, 3715, 3716};
 
-local banner_data = {
-{ 182527, 530, 253.54, 7083.81, 36.7728, -0.017453, 1},
-{ 182528, 530, 253.54, 7083.81, 36.7728, -0.017453, 3},
-{ 182529, 530, 253.54, 7083.81, 36.7728, -0.017453, 2};
-};
+PerformIngameSpawn(2,N_BANNER,530,253.54,7083.81,36.7728,-0.017453,200,0)
 
 function OnLoad(pGO)
 pGO:RegisterAIUpdateEvent(500)
@@ -97,7 +98,7 @@ end
 				pGO:SetWorldStateForZone(capturepoint_data[i][8],0)
 				a_t = a_t + 1
 				for k,v in pairs (GetPlayersInZone(ZONE_ZM))do
-					v:SendBroadcastMessage(""..capturepoint_data[i][12].." was taken by the alliance!|r")
+					v:SendBroadcastMessage("The "..capturepoint_data[i][12].." was taken by the alliance!|r")
 				end
 			elseif(capturepoint_data[i][2] <= BAR_STATUS_1 and pGO:GetWorldStateForZone(capturepoint_data[i][4])~=1 and pGO:GetDistanceYards(pGO:GetClosestPlayer()) < 60)then
 				pGO:SetWorldStateForZone(capturepoint_data[i][3],0)
@@ -108,7 +109,7 @@ end
 				pGO:SetWorldStateForZone(capturepoint_data[i][8],0)
 				h_t = h_t + 1
 				for k,v in pairs (GetPlayersInZone(ZONE_ZM))do
-					v:SendBroadcastMessage(""..capturepoint_data[i][12].." was taken by the horde!|r")
+					v:SendBroadcastMessage("The "..capturepoint_data[i][12].." was taken by the horde!|r")
 				end
 			elseif(capturepoint_data[i][2] > BAR_STATUS_1 and capturepoint_data[i][2] < 100 - BAR_STATUS_1 and pGO:GetWorldStateForZone(capturepoint_data[i][3])==1 and pGO:GetDistanceYards(pGO:GetClosestPlayer()) < 60)then
 				pGO:SetWorldStateForZone(capturepoint_data[i][3],0)
@@ -119,7 +120,10 @@ end
 				pGO:SetWorldStateForZone(capturepoint_data[i][8],1)
 				a_t = a_t - 1
 				for k,v in pairs (GetPlayersInZone(ZONE_ZM))do
-					v:SendBroadcastMessage("The alliance lost "..capturepoint_data[i][12].."!|r")
+					v:SendBroadcastMessage("The alliance lost the "..capturepoint_data[i][12].."!|r")
+					if(v:HasAura(ZM_STANDARD_A))then
+						v:RemoveAura(ZM_STANDARD_A)
+					end
 				end
 			elseif(capturepoint_data[i][2] > BAR_STATUS_1 and capturepoint_data[i][2] < 100 - BAR_STATUS_1 and pGO:GetWorldStateForZone(capturepoint_data[i][4])==1 and pGO:GetDistanceYards(pGO:GetClosestPlayer()) < 60)then
 				pGO:SetWorldStateForZone(capturepoint_data[i][3],0)
@@ -130,7 +134,10 @@ end
 				pGO:SetWorldStateForZone(capturepoint_data[i][8],1)
 				h_t = h_t - 1
 				for k,v in pairs (GetPlayersInZone(ZONE_ZM))do
-					v:SendBroadcastMessage("The horde lost "..capturepoint_data[i][12].."!|r")
+					v:SendBroadcastMessage("The horde lost the "..capturepoint_data[i][12].."!|r")
+					if(v:HasAura(ZM_STANDARD_H))then
+						v:RemoveAura(ZM_STANDARD_H)
+					end
 				end
 			end
 		end
@@ -151,7 +158,111 @@ end
 vars.plrvall = 0
 end
 
+function BannerOnLoad(pGO)
+if(pGO:GetEntry()==A_BANNER and pGO:GetWorldStateForZone(WORLDSTATE_ZM_TWINSPIRE_GY_ALLIANCE)~=1)then
+	pGO:Despawn(1,0)
+elseif(pGO:GetEntry()==H_BANNER and pGO:GetWorldStateForZone(WORLDSTATE_ZM_TWINSPIRE_GY_HORDE)~=1)then
+	pGO:Despawn(1,0)
+elseif(pGO:GetEntry()==N_BANNER and pGO:GetWorldStateForZone(WORLDSTATE_ZM_TWINSPIRE_GY_NEUTRAL)~=1)then
+	pGO:Despawn(1,0)
+end
+end
+
+local ZM_STANDARD_A =	32430
+local ZM_STANDARD_H =	32431
+
+function OnUse(pGO, event, pPlayer)
+rebuff = false
+if(pGO:GetEntry() == N_BANNER and pPlayer:HasAura(ZM_STANDARD_H) and pGO:GetWorldStateForZone(WORLDSTATE_ZM_TWINSPIRE_GY_NEUTRAL)==1 and h_t == 2)then
+	pGO:SetWorldStateForZone(WORLDSTATE_ZM_TWINSPIRE_GY_ALLIANCE,0)
+	pGO:SetWorldStateForZone(WORLDSTATE_ZM_TWINSPIRE_GY_HORDE,1)
+	pGO:SetWorldStateForZone(WORLDSTATE_ZM_TWINSPIRE_GY_NEUTRAL,0)
+	PerformIngameSpawn(2,H_BANNER,530,253.54,7083.81,36.7728,-0.017453,200,0)
+	controll_team = 1
+	rebuff = true
+	pGO:Despawn(1,0)
+elseif(pGO:GetEntry() == N_BANNER and pPlayer:HasAura(ZM_STANDARD_A) and pGO:GetWorldStateForZone(WORLDSTATE_ZM_TWINSPIRE_GY_NEUTRAL)==1 and a_t == 2)then
+	pGO:SetWorldStateForZone(WORLDSTATE_ZM_TWINSPIRE_GY_ALLIANCE,1)
+	pGO:SetWorldStateForZone(WORLDSTATE_ZM_TWINSPIRE_GY_HORDE,0)
+	pGO:SetWorldStateForZone(WORLDSTATE_ZM_TWINSPIRE_GY_NEUTRAL,0)
+	PerformIngameSpawn(2,A_BANNER,530,253.54,7083.81,36.7728,-0.017453,200,0)
+	controll_team = 0
+	rebuff = true
+	pGO:Despawn(1,0)
+elseif(pGO:GetEntry() == H_BANNER and pPlayer:HasAura(ZM_STANDARD_A) and pGO:GetWorldStateForZone(WORLDSTATE_ZM_TWINSPIRE_GY_HORDE)==1 and a_t == 2)then
+	pGO:SetWorldStateForZone(WORLDSTATE_ZM_TWINSPIRE_GY_ALLIANCE,1)
+	pGO:SetWorldStateForZone(WORLDSTATE_ZM_TWINSPIRE_GY_HORDE,0)
+	pGO:SetWorldStateForZone(WORLDSTATE_ZM_TWINSPIRE_GY_NEUTRAL,0)
+	PerformIngameSpawn(2,A_BANNER,530,253.54,7083.81,36.7728,-0.017453,200,0)
+	controll_team = 0
+	rebuff = true
+	pGO:Despawn(1,0)
+elseif(pGO:GetEntry() == A_BANNER and pPlayer:HasAura(ZM_STANDARD_H) and pGO:GetWorldStateForZone(WORLDSTATE_ZM_TWINSPIRE_GY_ALLIANCE)==1 and h_t == 2)then
+	pGO:SetWorldStateForZone(WORLDSTATE_ZM_TWINSPIRE_GY_ALLIANCE,0)
+	pGO:SetWorldStateForZone(WORLDSTATE_ZM_TWINSPIRE_GY_HORDE,1)
+	pGO:SetWorldStateForZone(WORLDSTATE_ZM_TWINSPIRE_GY_NEUTRAL,0)
+	PerformIngameSpawn(2,H_BANNER,530,253.54,7083.81,36.7728,-0.017453,200,0)
+	controll_team = 1
+	rebuff = true
+	pGO:Despawn(1,0)
+end
+if(rebuff == true)then
+	for i = 1, #buff_zone do
+		for k,v in pairs(GetPlayersInZone(buff_zone[i]))do
+			if(v:GetTeam() == controll_team and not v:HasAura(ZM_BUFF))then
+				v:AddAura(ZM_BUFF,0)
+			elseif(v:GetTeam() ~= controll_team and v:HasAura(ZM_BUFF))then
+				RemoveAura()
+			end
+			if(v:HasAura(ZM_STANDARD_H))then
+				v:RemoveAura(ZM_STANDARD_H)
+			end
+		end
+	end
+rebuff = false
+end
+end
+
+function OnZone(event, pPlayer, ZoneId, OldZoneId)
+buffed_zm = false
+for i = 1, #buff_zone do
+if(ZoneId == buff_zone[i])then
+	buffed_zm = true
+end
+end
+if(buffed_zm == true and not pPlayer:HasAura(ZM_BUFF) and pPlayer:GetTeam() == controll_team)then
+	pPlayer:AddAura(ZM_BUFF,0)
+elseif((buffed_zm == false or pPlayer:GetTeam() ~= controll_team) and pPlayer:HasAura(ZM_BUFF))then
+	pPlayer:RemoveAura(ZM_BUFF)
+end
+buffed_zm = false
+end
+
+function OnEnter(event, pPlayer)
+ZoneId = pPlayer:GetZoneId()
+buffed_zm = false
+for i = 1, #buff_zone do
+if(ZoneId == buff_zone[i])then
+	buffed_zm = true
+end
+end
+if(buffed_zm == true and not pPlayer:HasAura(ZM_BUFF) and pPlayer:GetTeam() == controll_team)then
+	pPlayer:AddAura(ZM_BUFF,0)
+elseif((buffed_zm == false or pPlayer:GetTeam() ~= controll_team) and pPlayer:HasAura(ZM_BUFF))then
+	pPlayer:RemoveAura(ZM_BUFF)
+end
+buffed_zm = false
+end
+
 for i = 1, #capturepoint_data do
 RegisterGameObjectEvent(capturepoint_data[i][1],5,AIUpdate)
 RegisterGameObjectEvent(capturepoint_data[i][1],2,OnLoad)
 end
+RegisterGameObjectEvent(A_BANNER,2,BannerOnLoad)
+RegisterGameObjectEvent(H_BANNER,2,BannerOnLoad)
+RegisterGameObjectEvent(N_BANNER,2,BannerOnLoad)
+RegisterGameObjectEvent(A_BANNER,4,OnUse)
+RegisterGameObjectEvent(H_BANNER,4,OnUse)
+RegisterGameObjectEvent(N_BANNER,4,OnUse)
+RegisterServerHook(15,OnZone)
+RegisterServerHook(4,OnEnter)
