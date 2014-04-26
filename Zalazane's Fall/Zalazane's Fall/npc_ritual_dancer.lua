630e322cbbcad4a2320061206d546b128311a56a
DarkAngel <darkangel39@abv.bg>
2014-04-26 11:49:07 +0300

    This is how you do this.

--- Zalazane's Fall/Zalazane's Fall/npc_ritual_dancer.lua
+++ Zalazane's Fall/Zalazane's Fall/npc_ritual_dancer.lua
@@ -1,27 +1,9 @@
-SPELL_TIKI_VISUAL_1 = 75214
-SPELL_TIKI_VISUAL_2 = 75228
-SPELL_TIKI_VISUAL_3 = 75229
-SPELL_TIKI_VISUAL_4 = 75219
-SPELL_TIKI_VISUAL_5 = 75230
-SPELL_TIKI_VISUAL_6 = 75220
-NPC_RITUAL_DANCER = 40356
+local spell = {75214,75228,75229,75219,75230,75220,40356}
+local NPC_RITUAL_DANCER = 40356
 
 function OnSpawn(pUnit, event)
 pUnit:RemoveAllAuras()
-local aura = math.random(1,6)
-if(aura == 1)then
-	pUnit:CastSpell(SPELL_TIKI_VISUAL_1)
-elseif(aura == 2)then
-	pUnit:CastSpell(SPELL_TIKI_VISUAL_2)
-elseif(aura == 3)then
-	pUnit:CastSpell(SPELL_TIKI_VISUAL_3)
-elseif(aura == 4)then
-	pUnit:CastSpell(SPELL_TIKI_VISUAL_4)
-elseif(aura == 5)then
-	pUnit:CastSpell(SPELL_TIKI_VISUAL_5)
-elseif(aura == 6)then
-	pUnit:CastSpell(SPELL_TIKI_VISUAL_6)
-end
+pUnit:CastSpell(spell[math.random(1,7)])
 end
 
 RegisterUnitEvent(NPC_RITUAL_DANCER,18,OnSpawn)
