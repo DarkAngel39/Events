bcc25e3b0953815144f224a0051f8d7cba9ab461
DarkAngel <darkangel39@abv.bg>
2014-08-22 23:19:41 +0300

    Zalazane's Fall: Updated spawn id's method.

--- Zalazane's Fall/Zalazane's Fall Despawn.sql
+++ Zalazane's Fall/Zalazane's Fall Despawn.sql
@@ -1,5 +1,8 @@
-DELETE FROM `creature_spawns` WHERE `id` BETWEEN '410000' AND '410141';
-DELETE FROM `gameobject_spawns` WHERE `id` BETWEEN 5900079 AND 5900156;
+SET @CGUID := 409999;
+SET @GGUID := 5900078;
+
+DELETE FROM `creature_spawns` WHERE `id` BETWEEN @CGUID+1 AND @CGUID+142;
+DELETE FROM `gameobject_spawns` WHERE `id` BETWEEN @GGUID+1 AND @GGUID+78;
 
 CREATE TABLE IF NOT EXISTS `active_event_id` (
   `active_event` int(10) unsigned NOT NULL,
