05587ec8e578d18c3ccbae99035e0721fb2d26fe
DarkAngel <darkangel39@abv.bg>
2014-08-24 23:36:07 +0300

    Night Elf Mohawk: Updated spawn id's method.

--- Night Elf Mohawk/Night Elf Mohawk Despawn.sql
+++ Night Elf Mohawk/Night Elf Mohawk Despawn.sql
@@ -1,5 +1,8 @@
-DELETE FROM `creature_spawns` WHERE `id` BETWEEN '434300' AND '434315';
-DELETE FROM `gameobject_spawns` WHERE `id` BETWEEN '512571' AND '512586';
+SET @CGUID:= 434299;
+SET @GGUID:= 512570;
+
+DELETE FROM `creature_spawns` WHERE `id` BETWEEN @CGUID+1 AND @CGUID+16;
+DELETE FROM `gameobject_spawns` WHERE `id` BETWEEN @GGUID+1 AND @GGUID+16;
 
 CREATE TABLE IF NOT EXISTS `active_event_id` (
   `active_event` int(10) unsigned NOT NULL,
