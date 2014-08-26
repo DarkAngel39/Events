688322e17f26ecc0821c3d0bc90333275f832c72
DarkAngel <darkangel39@abv.bg>
2014-08-26 11:08:54 +0300

    Some cleanup.

--- Call to Arms/Call to Arms Warsong Gulch!/Call to Arms Warsong Gulch! Despawn.sql
+++ Call to Arms/Call to Arms Warsong Gulch!/Call to Arms Warsong Gulch! Despawn.sql
@@ -4,9 +4,6 @@ SET @GGUID:= 512744;
 DELETE FROM `creature_spawns` WHERE `id` BETWEEN @CGUID+1 AND @CGUID+127;
 DELETE FROM `gameobject_spawns` WHERE `id` BETWEEN @GGUID+1 AND @GGUID+85;
 
-DELETE FROM `creature_spawns` WHERE `id` BETWEEN 434662 AND 434788;
-DELETE FROM `gameobject_spawns` WHERE `id` BETWEEN 512745 AND 512829;
-
 CREATE TABLE IF NOT EXISTS `active_event_id` (
   `active_event` int(10) unsigned NOT NULL,
   `name` text COLLATE latin1_bin NOT NULL,
